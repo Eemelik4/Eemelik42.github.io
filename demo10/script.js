934d1ea304671812c60b291e13c44567ba544010
Eemelik4 <99165656+Eemelik4@users.noreply.github.com>
2022-03-11 14:47:14 +0200

    Pieni siivous
    Kirjoitusvirheiden korjauksia ja pieniä parannuksia

--- demo10/script.js
+++ demo10/script.js
@@ -3,22 +3,20 @@ let kinkkuPizzaYhteishinta; // Luodaan muuttuja johon tallennetaan kinkku pizzoj
 function kinkkuFunktio() {
   var onkoKinkkuValittu; // Muuttuja johon tallennetaan tieto onko checkboxissa rasti
   onkoKinkkuValittu = document.getElementById("kinkkuananas"); // Hankkii tiedon onko checkboxissa rasti
-  if (onkoKinkkuValittu.checked) { // JOS checkboxissa on rasti ja määränä ei ole 0 niin seuraavat asiat suoritetaan
-    document.getElementById("kinkku").innerHTML = kinkkuAnanasMaara.value + " kinkkuananas pizzaa lisätty"; // Tulostaa montako kinkkuananas pizzaa lisättiin
+  if (onkoKinkkuValittu.checked) { // JOS checkboxissa on rasti niin seuraavat asiat suoritetaan   
     kinkkuPizzaYhteishinta = kinkkuAnanasMaara.value * 8; // Laskee pizzojen määrän * hinta ja tallentaa ne muuttujaan
-    document.getElementById("ostoskorinSumma").innerHTML = kinkkuPizzaYhteishinta + " euroa"; // Tulostaa yhteishinnan
+    document.getElementById("kinkku").innerHTML = kinkkuAnanasMaara.value + " kinkkuananaspizzaa lisätty joka on yhteensä " + kinkkuPizzaYhteishinta + " €"; // Tulostaa pizzat ja niiden yhteishinnan
   } else { 
-    document.getElementById("kinkku").innerHTML = "Virhe..."; // Tulostaa jos käyttäjä syötti jotain mitä ei olisi saanut
+    document.getElementById("kinkku").innerHTML = "Virhe..."; // Tulostaa jos käyttäjä syötti jotain muuta mitä ei olisi saanut
   }
 }
 let juustoPizzaYhteishinta;
+var onkoJuustoValittu;
 function juustoFunktio() {
-  var onkoJuustoValittu;
   onkoJuustoValittu = document.getElementById("juustoPizza");
   if (onkoJuustoValittu.checked) {
-    document.getElementById("juusto").innerHTML = juustoPizzaMaara.value + " juustopizzaa lisätty ostoskoriin";
     juustoPizzaYhteishinta = juustoPizzaMaara.value * 5;
-    document.getElementById("juustoKorinSumma").innerHTML = juustoPizzaYhteishinta + " euroa";
+    document.getElementById("juusto").innerHTML = juustoPizzaMaara.value + " juustopizzaa lisätty joka on yhteensä " + juustoPizzaYhteishinta + " €";
   } else {
     document.getElementById("juusto").innerHTML = "Virhe...";
   }
@@ -28,9 +26,8 @@ function laskeYhteen() { // Funktio joka laskee pizzojen hinnat yhteen
   kaikenYhteisHinta = kinkkuPizzaYhteishinta + juustoPizzaYhteishinta;
   document.getElementById("kaikenYhteisHinta").innerHTML = "Tilasit " + kinkkuAnanasMaara.value + " kinkkuananas pizzaa ja " + juustoPizzaMaara.value + " juustopizzaa joiden yhteishinta on " + kaikenYhteisHinta + " euroa";
 }
-
 function kotiinKuljetus() { // Jos käyttäjä valitsee kuljetuksen
-  document.getElementById("kuljetus").innerHTML = "Kotiinkuljetus. Kuljetusaika on noin " + kaikenYhteisHinta * 0.5 + " min. Mihin toimitamme tämän?";
+  document.getElementById("kuljetus").innerHTML = "Kotiinkuljetus. Kuljetusaika on noin " + kaikenYhteisHinta * 0.5 + " min. Mihin toimitamme tämän?"; // kaikenYhteisHinta * 0.5 on toimitusaika
 }
 function tilausNouto() { // Jos käyttäjä valitsee noudon
   document.getElementById("kuljetus").innerHTML = "Nouto";
